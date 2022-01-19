03a8756c88329575a9c27ccbf70df87293a95505
Ramin-Guliyev <ra1000ishere@outlook.com>
2022-01-19 20:05:32 +0400

    Updated MakeUp to more user friendly usage

---
+++ src/MakeUp/MakeUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MakeUp
+{
+    public static class MakeUp
+    {
+        public static void Write(this string? value,MakeUpColor color)
+        {
+            Console.ForegroundColor = (ConsoleColor)color;
+            Console.Write(value);
+            Console.ResetColor();
+        }
+        public static void WriteLine(this string? value,MakeUpColor color)
+        {
+            Console.ForegroundColor = (ConsoleColor)color;
+            Console.WriteLine(value);
+            Console.ResetColor();
+        }
+    }
+}
